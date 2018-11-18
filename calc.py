a1d9ee4c40c1a623e0cea7b2384ab39152afa9b3
son-vyas <soniyavyas29@gmail.com>
2018-11-18 19:47:35 +0530

    Added code for main

--- calc.py
+++ calc.py
@@ -8,15 +8,31 @@ class Calc:
         return self.a + self.b
     def sub(self):
         return self.a - self.b
-    def mul():
+    def mul(self):
         return self.a * self.b
-    def div():
+    def div(self):
         return self.a / self.b
 
 c = Calc()
 op=['+','-','*','/']
-while True:
+flag=True
+while flag:
     print('1. + \t2. - \t3. * \t4. / ')
+    x=int(raw_input())
+    for index,value in enumerate(op):
+         if index == x-1:
+             if value == '+':
+                 res=c.add()
+                 flag=False
 
-sum = c.add()
-print sum
+             elif value == '-':
+                 res=c.sub()
+                 flag=False
+
+             elif value == '*':
+                 res=c.mul()
+                 flag=False
+             elif value == '/':
+                 res=c.div()
+                 flag=False
+print res
