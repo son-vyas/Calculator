4b81c820c7c9ec1e7b73a0398c26bc75193fd7c3
son-vyas <soniyavyas29@gmail.com>
2018-11-21 13:36:53 +0530

    Added code for functions

--- calc_4.py
+++ calc_4.py
@@ -19,6 +19,17 @@ def menu():
 def add(number1,number2):
     return number1+number2
 
+def sub(number1,number2):
+    return number1-number2
+
+def mul(number1,number2):
+    return number1*number2
+
+def div(number1,number2):
+    return number1/number2
+
+welcome_msg()
+menu()
 if operation == '+':
     print ('{} + {}'.format(number1,number2))
     print (add(number1,number2))
@@ -36,4 +47,4 @@ elif operation =='/':
     print(div(number1/number2))
 
 else:
-    again()
+    menu()
